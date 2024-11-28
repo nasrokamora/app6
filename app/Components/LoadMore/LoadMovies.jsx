5b26a97fb5d351f500ac887c0f6b680030ea0e90
Ab_Nas <116016564+nasrokamora@users.noreply.github.com>
2024-11-28 17:34:01 +0000

    modified:   app/Components/LoadMore/LoadMovies.jsx

--- app/Components/LoadMore/LoadMovies.jsx
+++ app/Components/LoadMore/LoadMovies.jsx
@@ -6,16 +6,16 @@ import Image from "next/image"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
 } from "@/components/ui/hover-card"
 import { urlImage } from "@/app/libs/DataFetching"
 import BlurFade from "@/components/ui/blur-fade"
 async function getMoviesWithPage(page) {
     const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.NEXT_PUBLIC_API_KEY}&page=${page}`, {
-        next:{
-            revalidate:120
+        next: {
+            revalidate: 120
         }
     })
     if (!response.ok)
@@ -67,37 +67,37 @@ export default function LoadMovies() {
             loader={<h4 className="">Loading...</h4>}
             endMessage={<p className="text-2xl text-center">No more movies to show</p>}
         >
-                <div className="flex items-center justify-center mt-8 text-3xl font-semibold ">
-                    <h1>Explore all movies on Magix</h1>
-                </div>
+            <div className="flex items-center justify-center mt-8 text-3xl font-semibold ">
+                <h1>Explore all movies on Magix</h1>
+            </div>
             <div className="grid grid-cols-6 gap-8 p-8 md:grid-cols-3 lg:grid-cols-4">
                 {dataMovies.map((movie, index) => (
                     <BlurFade key={movie.id - index} delay={0.10 + index * 0.05} inView>
 
-                    <div key={index} className="relative flex flex-col items-center justify-center overflow-hidden hover:scale-110 hover:duration-300">
+                        <div key={index} className="relative flex flex-col items-center justify-center overflow-hidden hover:scale-110 hover:duration-300">
 
-                        <Link href={`/movies/list/${movie.id}` } >
+                            <Link href={`/movies/list/${movie.id}`} >
 
-                            <Image src={`${urlImage}/${movie.poster_path}`}
-                                alt={movie.title}
-                                width={200}
-                                height={150}
-                                style={{width:"auto"}}
-                                className="rounded-md "
-                                loading="eager"
-                                priority={true}
+                                <Image src={`${urlImage}/${movie.poster_path}`}
+                                    alt={movie.title}
+                                    width={200}
+                                    height={150}
+                                    style={{ width: "auto" }}
+                                    className="rounded-md "
+                                    loading="eager"
+                                    priority={true}
                                 />
 
 
-                            <h1 className="font-bold ">
-                                {movie.title.length > 14 ? movie.title.slice(0, 14) + "..." : movie.title}
-                            </h1>
+                                <h1 className="font-bold ">
+                                    {movie.title.length > 14 ? movie.title.slice(0, 14) + "..." : movie.title}
+                                </h1>
 
-                        </Link>
+                            </Link>
 
 
-                    </div>
-                                </BlurFade>
+                        </div>
+                    </BlurFade>
 
                 ))}
             </div>

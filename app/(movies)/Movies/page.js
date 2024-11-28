aeaf3964f19aa75fb521f9e64dcc7eead734c6c7
Ab_Nas <116016564+nasrokamora@users.noreply.github.com>
2024-11-28 17:49:11 +0000

    modified:   app/(movies)/Movies/page.js 	modified:   app/(tv)/Tv/List/[id]/season/[season_number]/page.js

--- app/(movies)/Movies/page.js
+++ app/(movies)/Movies/page.js
@@ -2,14 +2,15 @@ import LoadMovies from "@/app/Components/LoadMore/LoadMovies";
 import ToggleButton from "../Components/ToggleButton/ToggleButton";
 
 export const metadata = {
-    title:'Movies'
+    title: 'Movies'
 }
 
 
 export default function Movies() {
- 
+
     return (
         <div className=" w-full h-auto">
+
             <LoadMovies />
             <ToggleButton />
         </div>

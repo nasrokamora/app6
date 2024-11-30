689e859e3a4fa0af367f79a3d7892a97d35b10b5
Ab_Nas <116016564+nasrokamora@users.noreply.github.com>
2024-11-30 19:38:10 +0000

    modified:   app/Components/Footer/Footer.jsx 	modified:   app/page.js 	new file:   public/image/tmdb-image.png

--- app/page.js
+++ app/page.js
@@ -9,9 +9,9 @@ import AnimateScroll from "./Animations/AnimationNumber/AnimateScroll/AnimationS
 import styles from './styles/Animat.module.css'
 import PopularPerson from "./Components/PopularPerson/PopularPerson"
 import PaginationMovies from "./(movies)/Components/PaginationMovies/PaginationMovies"
-
-
-
+import { MdStars } from "react-icons/md";
+import { Clapperboard, ClapperboardIcon } from 'lucide-react';
+import { GiPopcorn } from "react-icons/gi";
 
 
 
@@ -66,7 +66,7 @@ export default async function Home() {
       {/*   section Tv & series & season */}
       <div className="ml-6 md:ml-0  mt-7 md:flex md:justify-center md:items-center ">
         <h1 className="text-4xl font-extrabold  scroll-m-20 lg:text-3xl md:text-2xl ">
-        <span className=" text-[#1977cd]">Must-Watch</span>   TV Shows
+        <span className=" text-[#1977cd]">Must <span><ClapperboardIcon className=" inline text-[#1977cd] md:size-6"  /> </span>Watch</span>   TV Shows 
         </h1>
       </div>
       <section className="mt-7">
@@ -75,9 +75,9 @@ export default async function Home() {
 
 
       {/* section tv genres */}
-      <div className=" ml-6   scroll-m-20 text-4xl font-extrabold  lg:text-3xl   md:flex md:justify-center md:items-center items-center mt-14 lg:mt-11 md:text-3xl md:ml-0 md:mt-5">
-        <h1 className="">
-        TV Shows by Genre
+      <div className=" ml-6   scroll-m-20 text-3xl font-extrabold  md:flex md:justify-center md:items-center md:flex-wrap md:text-center  mt-10 lg:mt-11 md:text-3xl md:ml-0 md:mt-8">
+        <h1 className=" relative bg-gradient-to-r from-[#000000] via-[#2044e2] to-[#000000] text-transparent bg-clip-text animate-gradient bg-[length:200%_auto] ">
+        TV Genres <span className="bg-gradient-to-r from-[#0062f4] via-[#000000] to-[#0062f4] text-transparent bg-clip-text animate-gradient bg-[length:200%_auto_]">Made for</span> <span className=" bg-gradient-to-r from-[#ffffff] via-[#2054e2] to-[#ffffff] text-transparent bg-clip-text animate-gradient bg-[length:200%_auto]">You</span><span className=""><GiPopcorn className="inline-block text-[#0065ff] absolute inset-0 -top-2 left-32 rotate-45" size={22} /></span>
         </h1>
       </div>
       <section className="mt-7">
@@ -86,9 +86,9 @@ export default async function Home() {
 
       {/* section popular person */}
       <section className=" mt-7">
-        <div className="ml-6 mt-7">
-          <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-3xl">
-            <span className=" text-blue-700">Most</span> popular
+        <div className="ml-6 mt-7 md:ml-0">
+          <h1 className="text-4xl font-extrabold  scroll-m-20 lg:text-3xl md:text-3xl md:text-center md:ml-0 ">
+            <span className=" text-[#ff007f]">Spotlight</span> on <span className=" bg-gradient-to-r from-[#ff007f] via-[#fffc40] to-[#ff007f] bg-[length:200%_auto] bg-clip-text text-transparent animate-gradient">Stars <span><MdStars className="inline text-[#fffc40]" size={28} /></span></span> 
           </h1>
         </div>
         <PopularPerson dataPersonPopular={dataPersonPopular.results} />

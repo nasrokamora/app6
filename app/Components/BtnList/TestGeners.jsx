5a585a1186fefd18d49930871f703be85a1a98cd
Ab_Nas <116016564+nasrokamora@users.noreply.github.com>
2025-02-23 20:17:49 +0000

    Refactor TestGeners component to enhance performance and improve user experience; optimize movie fetching and update loading indicators

--- app/Components/BtnList/TestGeners.jsx
+++ app/Components/BtnList/TestGeners.jsx
@@ -1,7 +1,7 @@
 "use client"
 import { IoStarSharp } from "react-icons/io5";
 import Image from "next/image";
-import React, { cache, memo, useCallback, useEffect, useRef, useState } from "react";
+import React, { cache, memo, useCallback, useEffect, useReducer, useRef, useState } from "react";
 import {
     Carousel,
     CarouselContent,
@@ -12,15 +12,76 @@ import {
 import { Button } from "@/components/ui/button";
 import LoadingGenreButton from "../LoadingUi/LoadingGenreList";
 import { MovieCarouselSkeleton } from "./LoadingSkeletonMovies";
+import { FaLink } from "react-icons/fa6";
+
+
+const initialState = {
+    genres: [],
+    isLoadingGenre: true,
+    movieList: [],
+    isLoading: true,
+    selectedGenre: null,
+    movieDetails: [],
+    error: null,
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "FETCH_GENRES_SUCCESS":
+            return {
+                ...state,
+                genres: action.payload,
+                isLoadingGenre: false
+            }
+        case "SET_SELECTED_GENRE":
+            return {
+                ...state,
+                selectedGenre: action.payload
+            }
+        case "FETCH_MOVIES_SUCCESS":
+            return {
+                ...state,
+                movieList: action.payload,
+                isLoading: false
+            }
+        case "FETCH_MOVIES_DETAILS_SUCCESS":
+            return {
+                ...state,
+                movieDetails: action.payload,
+                isLoading: false
+            }
+        case "SET_LOADING":
+            return {
+                ...state,
+                isLoading: action.payload
+            }
+        case "ERROR":
+            return {
+                ...state,
+                error: action.payload,
+                isLoading: false
+            }
+    }
+}
+
+
+
 
 export default function TestGeners() {
-    const [genres, setGenres] = useState([]);
-    const [isLoadingGenre, setIsLoadingGenre] = useState(true);
-    const [movieList, setMovieList] = useState([]);
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [selectedGenre, setSelectedGenre] = useState(null)
-    const [movieDetails, setMovieDetails] = useState([])
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+
+
+
+
+    // const [genres, setGenres] = useState([]);
+    // const [isLoadingGenre, setIsLoadingGenre] = useState(true);
+    // const [movieList, setMovieList] = useState([]);
+    // const [error, setError] = useState(null);
+    // const [isLoading, setIsLoading] = useState(true);
+    // const [selectedGenre, setSelectedGenre] = useState(null)
+    // const [movieDetails, setMovieDetails] = useState([])
 
     const [api, setApi] = useState();
     const [current, setCurrent] = useState(0);
@@ -41,26 +102,29 @@ export default function TestGeners() {
             if (!response.ok) {
                 throw new Error(data.message || "An error occurred while fetching data.")
             }
-            setGenres(data.genres)
+
+            dispatch({ type: "FETCH_GENRES_SUCCESS", payload: data.genres })
+
             if (data.genres.length > 0) {
                 fetchMovies(data.genres[0].id)
-                setSelectedGenre(data.genres[0].id)
-                setIsLoadingGenre(false)
+                dispatch({ type: "SET_SELECTED_GENRE", payload: data.genres[0].id })
+
+                // setIsLoadingGenre(false)
             }
         } catch (error) {
-            if (process.env.NODE_ENV !== "production") {
-                console.error(error, "Error fetch data Genres")
-            }
-            return { error: true, message: process.env.NODE_ENV === "production" ? "An unexpected error occurred, please try again." : error.message };
+            dispatch({ type: "ERROR", payload: error.message || "An unexpected error occurred" })
         }
     }
 
 
     const fetchMovies = useCallback(async (genreId) => {
-        if(cacheRef.current[genreId]) {
-            setMovieList(cacheRef.current[genreId])
+        if (cacheRef.current[genreId]) {
+            dispatch({ type: "FETCH_MOVIES_SUCCESS", payload: cacheRef.current[genreId] })
             return
         }
+
+        dispatch({ type: "SET_LOADING", payload: true })
+
         try {
             const response = await fetch(`/api/fetchMoviesWithGenres?genreId=${genreId}`,)
             const data = await response.json()
@@ -68,62 +132,39 @@ export default function TestGeners() {
             if (!response.ok) {
                 throw new Error(data.message || "An error occurred while fetching data.")
             }
-            setMovieList(data.results)
+            dispatch({ type: "FETCH_MOVIES_SUCCESS", payload: data.results })
+            // setMovieList(data.results)
             cacheRef.current[genreId] = data.results
+
             const details = await Promise.all(data.results.map(async (movie) => {
+
                 const response = await fetch(`/api/getMoviesById?movieId=${movie.id}`,)
+
                 const dataDetails = await response.json()
-                if(!response.ok) {
+
+                if (!response.ok) {
                     throw new Error(dataDetails.message || "An error occurred while fetching data.")
                 }
+
                 return dataDetails
             }))
-
-            setMovieDetails(details)
-
-            setIsLoading(false)
-            setError(null)
+            dispatch({ type: "FETCH_MOVIES_DETAILS_SUCCESS", payload: details })
 
         } catch (error) {
-            if (process.env.NODE_ENV !== "production") {
-                console.error(error, "Error fetch data Genres")
-            }
-            setError(error.message || "An unexpected error occurred");
+            dispatch({ type: "ERROR", payload: error.message || "An unexpected error occurred" })
         }
     }, [])
 
-    // const fetchMoviesDetails = useCallback(async (movieId) => {
-    //     try {
-    //         const response = await fetch(`/api/getMoviesById?movieId=${movieId}`,)
-    //         const data = await response.json()
-    //         if (!response.ok) {
-    //             throw new Error(data.message || "An error occurred while fetching data.")
-    //         }
+    // console.log(state.movieDetails)
 
-    //         setMovieDetails(data)
-    //         setIsLoading(false)
-    //         setError(null)
-
-
-    //     } catch (error) {
-    //         if (process.env.NODE_ENV !== "production") {
-    //             console.error(error, "Error fetch data Genres")
-    //         }
-    //         setError(error.message || "An unexpected error occurred");
-    //     }
-    // }, [])
-
-
-
-    const handleClick = useCallback(async (genreId) => {
-        setIsLoading(true)
+    const handleClick = useCallback((genreId) => {
+        if (state.selectedGenre === genreId) return
+        dispatch({ type: "SET_SELECTED_GENRE", payload: genreId })
         fetchMovies(genreId)
-        // fetchMoviesDetails(genreId)
-        setSelectedGenre(genreId)
-        setIsLoadingGenre(false)
-    }, [fetchMovies])
 
-    console.log(movieDetails)
+    }, [fetchMovies, state.selectedGenre])
+
+
 
     useEffect(() => {
         if (!api) {
@@ -137,7 +178,57 @@ export default function TestGeners() {
             setCurrent(api.selectedScrollSnap() + 1)
         })
     }, [api])
-    // console.log(movieList)
+
+
+    const ColorTypes = (status) => {
+        switch (status) {
+            case "Released":
+                return "text-green-500"
+            case "Upcoming":
+                return "text-orange-500"
+            case "In Production":
+                return "text-blue-500"
+            case "Post Production":
+                return "text-pink-500"
+            default:
+                return "text-gray-500"
+        }
+    }
+    const ColorTypesGenres = (genre) => {
+        switch (genre) {
+            case "Action":
+                return "text-red-500"
+            case "Adventure":
+                return "text-yellow-500"
+            case "Animation":
+                return "text-green-500"
+            case "Comedy":
+                return "text-blue-500"
+            case "Crime":
+                return "text-pink-500"
+            case "Drama":
+                return "text-indigo-500"
+            case "Family":
+                return "text-cyan-500"
+            case "Fantasy":
+                return "text-purple-500"
+            case "Horror":
+                return "text-rose-500"
+            case "Mystery":
+                return "text-fuchsia-500"
+            case "Romance":
+                return "text-sky-500"
+            case "Thriller":
+                return "text-amber-500"
+            case "Western":
+                return "text-lime-500"
+            case "Science Fiction":
+                return "text-teal-500"
+            default:
+                return "text-white"
+        }
+    }
+
     return (
         <div className="w-full h-screen">
 
@@ -152,51 +243,63 @@ export default function TestGeners() {
                 <div className=" border border-red-700 w-full h-[50vh] md:h-screen">
                     {/* content of details movies */}
                     <CarouselContent className="">
-                        {isLoading ? (
+                        {state.isLoading ? (
                             <MovieCarouselSkeleton />
                         ) : (
-                            movieList.length > 0 ? (
-                                movieList.map((movie) => (
+
+                            state.movieList.map((movie, index) => {
+
+                                const details = state.movieDetails[index];
+                                return (
                                     <CarouselItem className="max-w-full  h-[50vh] w-full md:h-screen " key={movie.id} >
                                         <div className="flex justify-between h-[50vh] w-full ">
 
                                             <div className=" bg-transparent w-[60%]   p-4">
                                                 <h1 className="text-3xl font-bold text-center ">
-                                                    {movie.title ? movie.title : movie.original_title || " Unknown"}
+                                                    {movie.original_title || " Unknown"}
                                                 </h1>
-                                                <div className=" pt-4 font-bold">
+                                                <div className=" pt-4 font-bold ">
                                                     <div className=" flex justify-start gap-3 items-center">
-                                                        <div className="bg-white/50 backdrop-blur w-fit p-1 rounded-md flex justify-center gap-2 items-center">
+                                                        <div className="bg-white/30 backdrop-blur w-fit p-1 rounded-md flex justify-center gap-2 items-center">
 
                                                             <h1 className=" font-bold text-yellow-500 ">
-                                                                {movie.vote_average ? movie.vote_average.toFixed(1) : "No rating available"}
+                                                                {movie.vote_average?.toFixed(1) || "No rating available"}
                                                             </h1>
                                                             <span className=" text-yellow-500">
                                                                 <IoStarSharp />
                                                             </span>
                                                         </div>
                                                         {/* release date */}
-                                                        <div className="bg-white/50 backdrop-blur w-fit p-1 rounded-md">
-                                                            <h1>{new Date(movie.release_date).getFullYear()}</h1>
+                                                        <div className="bg-white/30 backdrop-blur w-fit p-1 rounded-md">
+                                                            <h1>{new Date(movie.release_date).getFullYear() || "Unknown"}</h1>
                                                         </div>
                                                         <div>
-                                                            <h1 className="bg-white/50 backdrop-blur w-fit p-1 rounded-md">
+                                                            <h1 className="bg-white/30 backdrop-blur w-fit p-1 rounded-md">
                                                                 {movie.original_language}
                                                             </h1>
                                                         </div>
-
-                                                        {/* details movies */}
-
-                                                        {movieDetails && movieDetails.length > 0 && movieDetails.some(detail => detail.id === movie.id) && (
-                                                            <div className="" key={movie.id}>
-                                                                <h1>home page {movieDetails.find(detail => detail.id === movie.id).homepage}</h1>
-                                                            </div>
-                                                        )}
-
-
+                                                        {/* status */}
+                                                        <div className={`${ColorTypes(details.status)} bg-white/30 backdrop-blur w-fit p-1 rounded-md`}>
+                                                            <h1>{details.status}</h1>
+                                                        </div>
                                                     </div>
+                                                    {/* details movies */}
+
+                                                    {details &&
+                                                        <div className=" flex justify-start gap-2 items-center pt-4">
+
+                                                            {/* genres */}
+                                                            <div className=" flex justify-start items-center gap-2">
+                                                                {details.genres.map((genre) => (
+                                                                    <div key={genre.id} className="bg-white/30 backdrop-blur w-fit p-1 rounded-md">
+                                                                        <h1 className={``}>{genre.name}</h1>
+                                                                    </div>
+                                                                ))}
+                                                            </div>
+                                                        </div>
+                                                    }
                                                     <p className="text-sm text-center text-muted-foreground font-semibold pt-4">
-                                                        {movie.overview ? movie.overview : "No description available"}
+                                                        {movie.overview || "No description available"}
                                                     </p>
 
 
@@ -216,14 +319,9 @@ export default function TestGeners() {
                                         </div>
 
                                     </CarouselItem>
-                                ))
-                            ) : (
-                                <div>
-
-                                </div>
-                            )
+                                )
+                            })
                         )}
-
                     </CarouselContent>
                 </div>
             </Carousel>
@@ -240,16 +338,15 @@ export default function TestGeners() {
                     }}
                     className="w-full max-w-5xl md:max-w-xl  2xl:max-w-7xl lg:max-w-4xl">
                     <CarouselContent className="-ml-1">
-                        {isLoadingGenre ? (
+                        {state.isLoadingGenre ? (
                             <div className='flex items-center justify-center w-full'>
                                 <LoadingGenreButton />
                             </div>
                         ) : (
                             <GenresListType
-                                isLoadingGenre={isLoadingGenre}
                                 handleClick={handleClick}
-                                genres={genres}
-                                selectedGenre={selectedGenre}
+                                state={state}
+
                             />
                         )
                         }
@@ -267,17 +364,16 @@ export default function TestGeners() {
 
 
 
-const GenresListType = React.memo(({ genres, selectedGenre, handleClick }) => {
+const GenresListType = React.memo(({ state, handleClick }) => {
 
     return (
-        genres.map((genre, index) => (
+        state.genres.map((genre) => (
             <CarouselItem key={genre.id} className="p-1 basis-1/7 lg:basis-1/8 md:basis-1/7 ">
-                <Button variant="outline" className={`2xl:text-xl  ${selectedGenre === genre.id ? 'text-red-700' : ' text-zinc-500'}`}
+                <Button variant="outline" className={`2xl:text-xl  ${state.selectedGenre === genre.id ? 'text-red-700' : ' text-zinc-500'}`}
                     onClick={() => handleClick(genre.id)}>{genre.name}</Button>
             </CarouselItem>
         ))
     )
-
-
-
 })
+
+

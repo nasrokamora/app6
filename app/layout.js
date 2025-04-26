b4fac81767b45237a47984b6b6cad4faf62e25dc
Ab_Nas <116016564+nasrokamora@users.noreply.github.com>
2025-04-26 20:24:34 +0100

    Add spacing after Nav component for improved layout consistency

--- app/layout.js
+++ app/layout.js
@@ -134,7 +134,8 @@ export default function RootLayout({ children }) {
           shadow="0 0 10px #a632aa,0 0 5px #9c39ed"
         />
         <JsonLdSchema />
-        <Nav />
+        <Nav /> 
+        
         {children}
 
         <Footer />
